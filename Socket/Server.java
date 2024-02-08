ae4d69e199d5f40dbdb9ae68a50d90734b6e8d5e
THEWEERND <149047512+leonardocasadei@users.noreply.github.com>
2024-02-08 12:08:02 +0100

    Update Server.java

--- Socket/Server.java
+++ Socket/Server.java
@@ -1,36 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package server;
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
-/**
- *
- * @author Studenti
- */
 public class Server {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException{
-        int port=12345;
+    public static void main(String[] args) throws IOException {
+        int port = 12345; // Porta sulla quale il server ascolta
         ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server in ascolto sulla porta  " + port);
-        
-        try{
-            while(true){
-                Socket clientSocket=serverSocket.accept();
-                System.out.println("Messaggio del client ricevuto");
+        System.out.println("Server in ascolto sulla porta " + port);
+
+        try {
+            while (true) {
+                // Stoppa l'esecuzione finché un client non si connette
+                Socket clientSocket = serverSocket.accept();
 
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                
                 out.println("Ciao client!");
                 clientSocket.close();
             }
-        } finally{
+        } finally {
             serverSocket.close();
         }
     }
