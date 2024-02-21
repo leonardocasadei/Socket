175041f789f0799f9f5caa71bafdc4ba60797e8b
THEWEERND <149047512+leonardocasadei@users.noreply.github.com>
2024-02-21 11:23:04 +0100

    Update Server.java

--- Socket/Server.java
+++ Socket/Server.java
@@ -1,20 +1,18 @@
+import java.io.IOException;
 import java.net.*;
-import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        int port = 12345; // Porta sulla quale il server ascolta
+        int port = 12345;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server in ascolto sulla porta " + port);
 
         try {
             while (true) {
-                // Stoppa l'esecuzione finché un client non si connette
                 Socket clientSocket = serverSocket.accept();
-
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.println("Ciao client!");
-                clientSocket.close();
+                System.out.println("Connessione accettata da " + clientSocket.getRemoteSocketAddress());
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                new Thread(clientHandler).start();
             }
         } finally {
             serverSocket.close();
