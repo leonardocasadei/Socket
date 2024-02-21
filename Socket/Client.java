45f7ee6bf063161ce51bfa7b1475c914047224c1
THEWEERND <149047512+leonardocasadei@users.noreply.github.com>
2024-02-21 11:23:44 +0100

    Update Client.java

--- Socket/Client.java
+++ Socket/Client.java
@@ -3,14 +3,22 @@ import java.io.*;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String hostName="localhost";
-        int port=12345;
+        String hostName = "localhost";
+        int port = 12345;
 
-        try(Socket socket = new Socket(hostName, port); BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            String fromServer;
-            while((fromServer=in.readLine()) != null){
-                System.out.println("Server: " + fromServer);
-                break;
+        try (Socket socket = new Socket(hostName, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server: " + in.readLine());
+
+                if (userInput.equalsIgnoreCase("exit")) {
+                    break;
+                }
             }
         }
     }
